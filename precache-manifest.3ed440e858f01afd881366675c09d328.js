2da86f184a494b8114be3d5464145c2657b7591f
rohitmuley <rohitmuley93@gmail.com>
2020-07-30 17:27:00 +0530

    Updates

--- precache-manifest.3ed440e858f01afd881366675c09d328.js
+++ precache-manifest.3ed440e858f01afd881366675c09d328.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "111c34e56ec146a4d45d9cb0461a1878",
+    "revision": "41de060194fb745745760f69e45f28e2",
     "url": "/learnReact/index.html"
   },
   {
-    "revision": "727d206c71913922e72a",
-    "url": "/learnReact/static/css/main.9a9afc6c.chunk.css"
+    "revision": "d968aa32fa6ceb405242",
+    "url": "/learnReact/static/css/main.9e90cd8f.chunk.css"
   },
   {
     "revision": "92f11f54a0c2747bf9ba",
@@ -16,15 +16,15 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/learnReact/static/js/2.90c4567a.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "727d206c71913922e72a",
-    "url": "/learnReact/static/js/main.6f499470.chunk.js"
+    "revision": "d968aa32fa6ceb405242",
+    "url": "/learnReact/static/js/main.384c80ca.chunk.js"
   },
   {
     "revision": "be7eca5e718aff5bdb4d",
     "url": "/learnReact/static/js/runtime-main.fb20a0d0.js"
   },
   {
-    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
-    "url": "/learnReact/static/media/logo.5d5d9eef.svg"
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/learnReact/static/media/logo.ee7cd8ed.svg"
   }
 ]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/learnReact/precache-manifest.9c1e30da8aec4b43dcbd2c8a4bd1053c.js"
+  "/learnReact/precache-manifest.3ed440e858f01afd881366675c09d328.js"
 );
 
 self.addEventListener('message', (event) => {
